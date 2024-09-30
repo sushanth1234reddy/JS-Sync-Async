141a60bc22fcf15486541a66c8cfe52b373d7c11
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-09-30 23:52:43 +0530

    Add files via upload

---
+++ synchronousOps.js
@@ -0,0 +1,5 @@
+//********JavaScript is a Single Threaded Synchronous language******
+//synchronous Operations: these are the sequnetial operations which require one operation to complete before the next operation to begin.
+console.log("1","hi");
+console.log("2","hello");
+console.log("3","welcome");
