a954fa0f00de50e919dbdb1da8fec55ee767af9a
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-09-30 23:53:36 +0530

    Add files via upload

---
+++ akshay.js
@@ -0,0 +1,10 @@
+//Asynchronous Operations: these are the non-blocking processes, which can run multiple tasks at a same time.
+setTimeout(() => {
+    console.log("RedFlower")
+}, 5000);  //delays the code execution for 5 Sec
+
+setTimeout(() => {
+    console.log("Akshay Reddy");
+}, 2000);   // delays the code execution for 2 Sec
+
+// console.log("3","welcome")
